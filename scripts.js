12bf85e0be213dcfa50f35f706adc0094212b153
leticia <lettax_05@hotmail.com>
2020-03-05 23:38:13 -0700

    glamm

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+});
